b760816d94e2853bfd7419b02ce1d43c57abe6a4
Diego Ferreira <diferreira.program@gmail.com>
2023-02-25 11:47:46 -0300

    update dpr

--- Controle_Caixa.dpr
+++ Controle_Caixa.dpr
@@ -2,46 +2,13 @@ program Controle_Caixa;
 
 uses
   Vcl.Forms,
-  Vcl.Controls,
-  uLogin in 'uLogin.pas' {FrmLogin},
-  uMain in 'View\uMain.pas' {FrmMain},
-  DM.Main in 'DM.Main.pas' {DMMain: TDataModule},
-  uCadastroRegistro in 'View\uCadastroRegistro.pas' {FrmCadastro},
-  uGridRegistro in 'View\uGridRegistro.pas' {FrmGridRegistro},
-  uRelCaixa in 'View\uRelCaixa.pas' {FrmRelCaixa},
-  uCadUsuario in 'View\uCadUsuario.pas' {FrmCadUser},
-  uGridUsers in 'View\uGridUsers.pas' {FrmGridUsers},
-  Manager.Interfaces in 'Manager\Manager.Interfaces.pas',
-  Manager.Types in 'Manager\Manager.Types.pas',
-  Manager.Caixa in 'Manager\Manager.Caixa.pas',
-  Manager.Usuario in 'Manager\Manager.Usuario.pas',
-  DTO.Usuario in 'Classes\DTO.Usuario.pas',
-  DTO.Caixa in 'Classes\DTO.Caixa.pas',
-  uMessage in 'View\uMessage.pas' {FrmShowMessage},
-  Fnc_Utils in 'Utils\Fnc_Utils.pas',
-  Controle.Caixa.Model.Connection.Interfaces in 'Manager\Model\Controle.Caixa.Model.Connection.Interfaces.pas',
-  Controle.Caixa.Model.Connection in 'Manager\Model\Controle.Caixa.Model.Connection.pas',
-  Controle.Caixa.Controller.Manager.Interfaces in 'Manager\Controller\Controle.Caixa.Controller.Manager.Interfaces.pas',
-  Controle.Caixa.Controller.Manager in 'Manager\Controller\Controle.Caixa.Controller.Manager.pas';
+  Unit1 in 'Unit1.pas' {Form1};
 
 {$R *.res}
-
-var
-  FLogin: TFrmLogin;
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-
-  Application.CreateForm(TDMMain, DMMain);
-  FLogin := TFrmLogin.Create(nil);
-  FLogin.ShowModal;
-
-  Application.CreateForm(TFrmMain, FrmMain);
-
-  FLogin.Hide;
-  FLogin.Free;
-
+  Application.CreateForm(TForm1, Form1);
   Application.Run;
-
 end.
